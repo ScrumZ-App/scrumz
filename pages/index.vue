f15aee009949414f59f9909b3f540824541b7f8a
Umutcan Ceyhan <umutcan.ceyhan@sabancidx.com>
2023-11-18 02:09:07 +0300

    feat: add page redirection

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <button>
+    <a href="/room">Create Room</a>
+    </button>
+
+<input type="text" name="roomId" id="room_id">
+    <button>
+    <a href="/room">Join Room</a>
+    
+    </button>
+</template>
+  
